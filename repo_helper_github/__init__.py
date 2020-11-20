098e0101a812e78d82939295ec1f0a81d75df4a4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-20 17:25:52 +0000

    Bump version v0.1.2 -> v0.2.0

--- repo_helper_github/__init__.py
+++ repo_helper_github/__init__.py
@@ -58,7 +58,7 @@ from repo_helper_github.options import token_option, version_callback
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.2"
+__version__: str = "0.2.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = [
